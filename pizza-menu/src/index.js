ae3ad3ff927555975bd5d3f19676134b6f481a19
= <kumarmridul91@gmail.com>
2023-07-07 22:59:26 +0530

    Adding CSS file to project

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,8 +1,99 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
+
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
 
 function App(){
-    return <h1>Hello World</h1>
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer/>
+        </div>
+    );
+}
+
+function Header() {
+  const style = {}
+  return (
+    <header className="header">
+      <h1>Fast Pizza React</h1>
+    </header>
+  );
+  
+}
+function Menu() {
+  return(
+    <main className="menu">
+      <h2>Menu</h2>
+      <Pizza 
+        name="Pizza spincani"
+        ingredients="Tomato Mozarella"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+      />
+    </main>
+  )
+}
+
+function Footer() {
+  return(<footer>{new Date().toLocaleTimeString()}.We're currently open</footer>)
+}
+
+function Pizza (props) {
+  console.log(props)
+    return (
+        <div className="footer">
+            <img src={props.photoName} alt={props.name}/>
+            <h3>{props.name}</h3>
+            <p>{props.ingredients}</p>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
